cb51014866b17b1773c6a1a66b9c02bdfc7af856
unknown <MSI@MSI-PC.(none)>
2012-07-09 15:03:21 +0200

    Der Rest

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,11 +2,16 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    protected function _initDoctype()
+	protected function _initDoctype()
+	{
+		$this->bootstrap('view');
+		$view = $this->getResource('view');
+		$view->doctype('XHTML1_STRICT');
+	}
+
+    protected function _initLoaderResources()
     {
-        $this->bootstrap('view');
-        $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
+        $this->getResourceLoader()->addResourceType('classes', 'classes/', 'Class');
     }
 }
 
