155dd6afed7e15a70b1e75f4ca7e2e5bc209bcc0
unknown <MSI@MSI-PC.(none)>
2012-07-09 18:36:18 +0200

    index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-
-// Das ist ein Testkommentar vom MAC
-
-require_once 'application/init.php';
-require_once 'Zend/Application.php';
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
-);
-$application->bootstrap()
-            ->run();
+header("Status: 301 Moved Permanently"); 
+header("Location: /public/"); 	
+?>
