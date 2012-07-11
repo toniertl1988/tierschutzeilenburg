5b257e1c699b74932b89453d9c0d96d2b62d5223
Alex <MSI@MSI-PC.(none)>
2012-07-11 19:19:48 +0200

    Facebook integriert

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -1,11 +1,6 @@
 <?php
 class IndexController extends Zend_Controller_Action
 {
-	public function init()
-	{
-		$this->_helper->layout->setLayout('adminlayout');
-	}
-	
 	public function indexAction()
 	{
 		echo "admin / index";
